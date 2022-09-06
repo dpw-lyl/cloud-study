13ff2c9a4273889d9034078c8b4994b384b7fead
dengpw <dengpw@octvision.com>
2022-09-06 17:40:45 +0800

    模块设计

---
+++ dpw-lyl-join-good-job-parent/dpw-lyl-good-job-iot/good-job-iot-web/src/main/java/com/dpw/lyl/join/good/job/iot/web/GoodJobIotWebApplication.java
@@ -0,0 +1,13 @@
+package com.dpw.lyl.join.good.job.iot.web;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class GoodJobIotWebApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(GoodJobIotWebApplication.class, args);
+	}
+
+}
