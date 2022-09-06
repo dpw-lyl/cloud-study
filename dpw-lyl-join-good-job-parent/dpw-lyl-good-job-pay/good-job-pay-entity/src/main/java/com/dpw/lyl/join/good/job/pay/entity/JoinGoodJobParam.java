6b5e68b0efa9b3fb3d547e926dd7d0f14f745896
dengpw <dengpw@octvision.com>
2022-09-06 12:02:46 +0800

    打包插件修改

--- dpw-lyl-join-good-job-parent/dpw-lyl-good-job-pay/good-job-pay-entity/src/main/java/com/dpw/lyl/join/good/job/pay/entity/JoinGoodJobParam.java
+++ dpw-lyl-join-good-job-parent/dpw-lyl-good-job-pay/good-job-pay-entity/src/main/java/com/dpw/lyl/join/good/job/pay/entity/JoinGoodJobParam.java
@@ -1,5 +1,6 @@
 package com.dpw.lyl.join.good.job.pay.entity;
 
+
 import com.dpw.lyl.join.good.job.foundation.base.BaseParam;
 import lombok.Data;
 import lombok.EqualsAndHashCode;
--- dpw-lyl-join-good-job-parent/dpw-lyl-good-job-pay/good-job-pay-server/src/test/java/com/dpw/lyl/join/good/job/pay/GoodJobPayServerApplicationTests.java
+++ dpw-lyl-join-good-job-parent/dpw-lyl-good-job-pay/good-job-pay-server/src/test/java/com/dpw/lyl/join/good/job/pay/GoodJobPayServerApplicationTests.java
@@ -1,4 +1,4 @@
-package com.dpw.lyl.join.good.job;
+package com.dpw.lyl.join.good.job.pay;
 
 import org.junit.jupiter.api.Test;
 import org.springframework.boot.test.context.SpringBootTest;
