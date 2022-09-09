b8af70cdc3e2380043666c8df0cc23e72f867439
dengpw <dengpw@octvision.com>
2022-09-09 16:17:34 +0800

    spring-boot,spring-cloud,alibaba-cloud,gateway版本整合

--- dpw-lyl-join-good-job-parent/dpw-lyl-good-job-iot/good-job-iot-web/src/main/java/com/dpw/lyl/join/good/job/iot/web/GoodJobIotWebApplication.java
+++ dpw-lyl-join-good-job-parent/dpw-lyl-good-job-iot/good-job-iot-web/src/main/java/com/dpw/lyl/join/good/job/iot/web/GoodJobIotWebApplication.java
@@ -2,7 +2,9 @@ package com.dpw.lyl.join.good.job.iot.web;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.cloud.client.discovery.EnableDiscoveryClient;
 
+@EnableDiscoveryClient
 @SpringBootApplication
 public class GoodJobIotWebApplication {
 
---
+++ dpw-lyl-join-good-job-parent/dpw-lyl-good-job-iot/good-job-iot-web/src/main/java/com/dpw/lyl/join/good/job/iot/web/controller/IotController.java
@@ -0,0 +1,17 @@
+package com.dpw.lyl.join.good.job.iot.web.controller;
+
+import lombok.extern.slf4j.Slf4j;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+/**
+ * @Author: dengpw
+ * @createTime: 2022年09月09日 14:58:07
+ * @version: 1.0.0
+ * @Description: 物联网测试入口
+ */
+@Slf4j
+@RestController
+@RequestMapping("/iot")
+public class IotController {
+}
