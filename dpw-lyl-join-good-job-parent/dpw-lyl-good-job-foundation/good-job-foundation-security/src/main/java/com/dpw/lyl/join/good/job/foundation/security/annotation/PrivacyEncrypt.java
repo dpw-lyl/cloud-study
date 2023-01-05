bbda957610793755a632fa43d5e11736654a1e81
dengpw <dengpw@octvision.com>
2023-01-05 09:26:38 +0800

    代码修改

--- dpw-lyl-join-good-job-parent/dpw-lyl-good-job-foundation/good-job-foundation-security/src/main/java/com/dpw/lyl/join/good/job/foundation/security/annotation/PrivacyEncrypt.java
+++ dpw-lyl-join-good-job-parent/dpw-lyl-good-job-foundation/good-job-foundation-security/src/main/java/com/dpw/lyl/join/good/job/foundation/security/annotation/PrivacyEncrypt.java
@@ -19,7 +19,7 @@ import java.lang.annotation.Target;
 @Retention(RetentionPolicy.RUNTIME) // class文件中保留，运行时也保留，能通过反射读取到
 @JacksonAnnotationsInside // 表示自定义自己的注解PrivacyEncrypt
 @JsonSerialize(using = PrivacySerializer.class) // 该注解使用序列化的方式
-public @ interface PrivacyEncrypt {
+public @interface PrivacyEncrypt {
     /**
      * 脱敏数据类型（没给默认值，所以使用时必须指定type）
      */
